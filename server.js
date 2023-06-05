1c128fd179ff235483da917f94849f28c6973238
NubScript7 <112324399+NubScript7@users.noreply.github.com>
2023-06-05 19:24:12 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+function uuidV4() {
+  return String(Math.trunc(Math.random()*999)).padStart(3,'0')
+}
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(3000)
